54132bfbfa13cfd36cde6836ec0306c2101e0665
Udong0 <91842264+Udong0@users.noreply.github.com>
2023-12-03 20:21:49 +0700

    Add files via upload

---
+++ CREATE DATABASE myITS_classroom.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE myITS_classroom
+
+CREATE Table Dosen(
+    
+);
+
+CREATE Table Mata_Kuliah(
+
+);
+
+CREATE Table Kelas(
+
+);
+
+CREATE Table Mahasiswa(
+
+);
+
+CREATE Table Private_File(
+
+);
+
+CREATE Table Sesi_Pertemuan(
+
+);
+
+CREATE Table Detail_Tugas(
+
+);
+
+CREATE Table File(
+
+);
+
+
+
