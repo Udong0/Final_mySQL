d36ddbba628103c8716777a9484784256f11559d
Udong0 <91842264+Udong0@users.noreply.github.com>
2023-12-03 21:07:35 +0700

    Update CREATE DATABASE myITS_classroom.sql

--- CREATE DATABASE myITS_classroom.sql
+++ CREATE DATABASE myITS_classroom.sql
@@ -1,37 +1,37 @@
 CREATE DATABASE myITS_classroom
 
 CREATE Table Dosen(
-    NIDN char(10) NOT NULL
-    dsn_nama VARCHAR(60) NOT NULL
-    dsn_email VARCHAR(100) NOT NULL
+    NIDN         char(10) NOT NULL
+    dsn_nama     VARCHAR(60) NOT NULL
+    dsn_email    VARCHAR(100) NOT NULL
     PRIMARY KEY (NIDN)
 );
 
 CREATE Table Mata_Kuliah(
-    mk_id char(8) NOT NULL
-    mk_nama VARCHAR(30) NOT NULL
-    mk_semester VARCHAR(5) NOT NULL
+    mk_id         char(8) NOT NULL
+    mk_nama       VARCHAR(30) NOT NULL
+    mk_semester   VARCHAR(5) NOT NULL
     PRIMARY KEY (mk_id)
 );
 
 CREATE Table Kelas(
-    kls_id char(8) NOT NULL
-    kls_nama VARCHAR(30) NOT NULL
+    kls_id     char(8) NOT NULL
+    kls_nama   VARCHAR(30) NOT NULL
     PRIMARY KEY (kls_id)
 );
 
 CREATE Table Mahasiswa(
-    nrp char(10) NOT NULL
-    mhs_nama VARCHAR(60) NOT NULL
-    mhs_email VARCHAR(100)  NOT NULL
-    mhs_jurusan VARCHAR(30) NOT NULL
+    nrp           char(10) NOT NULL
+    mhs_nama      VARCHAR(60) NOT NULL
+    mhs_email     VARCHAR(100)  NOT NULL
+    mhs_jurusan   VARCHAR(30) NOT NULL
     PRIMARY KEY (nrp)
 );
 
 CREATE Table Private_File(
-    pf_id char(8) NOT NULL
-    pf_nama VARCHAR(30) NOT NULL
-    pf_jenis VARCHAR(5) NOT NULL
+    pf_id             char(8) NOT NULL
+    pf_nama           VARCHAR(30) NOT NULL
+    pf_jenis          VARCHAR(5) NOT NULL
     pf_tanggal_upload datetime NOT NULL
 );
 
