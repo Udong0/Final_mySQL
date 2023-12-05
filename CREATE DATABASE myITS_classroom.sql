a00c0c0d05e8fe01cedb80d10ea053c718fb2ce7
Udong0 <91842264+Udong0@users.noreply.github.com>
2023-12-05 07:47:33 +0700

    Update CREATE DATABASE myITS_classroom.sql

--- CREATE DATABASE myITS_classroom.sql
+++ CREATE DATABASE myITS_classroom.sql
@@ -7,60 +7,22 @@ CREATE Table Dosen(
     PRIMARY KEY (NIDN)
 );
 
+CREATE Table Mahasiswa(
+    nrp char(10) NOT NULL,
+    mhs_nama VARCHAR(60) NOT NULL,
+    mhs_email VARCHAR(100)  NOT NULL,
+    mhs_jurusan VARCHAR(30) NOT NULL,
+    PRIMARY KEY (nrp)
+);
 
 CREATE Table Private_File(
     pf_id char(8) NOT NULL,
     pf_nama VARCHAR(30) NOT NULL,
     pf_jenis VARCHAR(5) NOT NULL,
     pf_tanggal_upload datetime NOT NULL,  
-    pf_mhs_nrp char(10) NOT NULL,
-    PRIMARY KEY (pf_id),
-    FOREIGN KEY (pf_mhs_nrp) REFERENCES Mahasiswa(mhs_nrp)
-);
-
-CREATE Table Mahasiswa(
-    mhs_nrp char(10) NOT NULL,
-    mhs_nama VARCHAR(60) NOT NULL,
-    mhs_email VARCHAR(100)  NOT NULL,
-    mhs_jurusan VARCHAR(30) NOT NULL,
-    PRIMARY KEY (mhs_nrp)
-);
-
-CREATE Table Files(
-    f_id char(10) NOT NULL,
-    f_nama int NOT NULL,
-    f_jenis VARCHAR(5) NOT NULL,
-    f_tanggal_upload datetime NOT NULL,
-    f_s_id char(8)     NOT NULL,
-    PRIMARY KEY (f_id),
-    FOREIGN KEY (f_s_id) REFERENCES Sesi_Pertemuan(s_id)
-
-);
-
-CREATE Table Sesi_Pertemuan(
-    s_id                char(8)     NOT NULL,
-    s_nama              VARCHAR(30) NOT NULL,
-    sp_deskripsi        Text,
-    sp_tanggal_dibuat   date        NOT NULL,
-    sp_file             VARCHAR(30),
-    sp_tugas            VARCHAR(30),
-    sp_lampiran         VARCHAR(30)
-    PRIMARY KEY (s_id),
-);
-
-CREATE Table Kelas(
-    kls_id char(8) NOT NULL,
-    kls_nama VARCHAR(30) NOT NULL,
-    Sesi_Pertemuan_s_ID char(8) NOT NULL,
-    PRIMARY KEY (kls_id),
-    FOREIGN KEY (Sesi_Pertemuan_s_ID) REFERENCES Sesi_Pertemuan(s_id)
-);
-
-CREATE Table Mahasiswa_Kelas(
     Mahasiswa_nrp char(10) NOT NULL,
-    Kelas_kls_ID char(8) NOT NULL,
-    FOREIGN KEY (Mahasiswa_nrp) REFERENCES Mahasiswa(mhs_nrp),
-    FOREIGN KEY (Kelas_kls_ID) REFERENCES Kelas(kls_id)
+    PRIMARY KEY (pf_id),
+    FOREIGN KEY (Mahasiswa_nrp) REFERENCES Mahasiswa(nrp)
 );
 
 CREATE Table Mata_Kuliah(
@@ -68,13 +30,51 @@ CREATE Table Mata_Kuliah(
     mk_nama VARCHAR(30) NOT NULL,
     mk_semester VARCHAR(5) NOT NULL,
     Kelas_kls_ID  char(8)  NOT NULL,
-    mk_NIDN char(10) NOT NULL,
+    Dosen_NIDN char(10) NOT NULL,
     PRIMARY KEY (mk_id),
-    FOREIGN KEY (Kelas_kls_ID) REFERENCES Kelas(kls_id),
-    FOREIGN KEY (mk_NIDN) REFERENCES Dosen(NIDN)
+    FOREIGN KEY (Dosen_NIDN) REFERENCES Dosen(NIDN)
 );
 
-CREATE Table Detail_Tugas(
+CREATE Table Kelas(
+    kls_id char(8) NOT NULL,
+    kls_nama VARCHAR(30) NOT NULL,
+    Mata_Kuliah_mk_ID char(8) NOT NULL,
+    PRIMARY KEY (kls_id),
+    FOREIGN KEY (Mata_Kuliah_mk_ID) REFERENCES Mata_Kuliah(mk_id)
+);
+
+CREATE Table Sesi_Pertemuan(
+    sp_id               char(8)     NOT NULL,
+    sp_nama             VARCHAR(30) NOT NULL,
+    sp_deskripsi        Text,
+    sp_tanggal_dibuat   date        NOT NULL,
+    sp_file             VARCHAR(30),
+    sp_tugas            VARCHAR(30),
+    sp_lampiran         VARCHAR(30),
+    Kelas_kls_ID        char(8),
+    PRIMARY KEY (sp_id),
+    FOREIGN KEY (Kelas_kls_ID) REFERENCES Kelas(kls_id)
+);
+
+CREATE Table Files(
+    f_id    char(10) NOT NULL,
+    f_nama int NOT NULL,
+    f_jenis VARCHAR(5) NOT NULL,
+    f_tanggal_upload datetime NOT NULL,
+    Sesi_Pertemuan_sp_id    char(8) NOT NULL,
+    PRIMARY KEY (f_id),
+    FOREIGN KEY (Sesi_Pertemuan_sp_id) REFERENCES Sesi_Pertemuan(sp_id)
+);
+
+CREATE Table Mahasiswa_Kelas(
+    Mahasiswa_nrp char(10) NOT NULL,
+    Kelas_kls_ID char(8) NOT NULL,
+    PRIMARY KEY (Mahasiswa_nrp, Kelas_kls_ID),
+    FOREIGN KEY (Mahasiswa_nrp) REFERENCES Mahasiswa(nrp),
+    FOREIGN KEY (Kelas_kls_ID) REFERENCES Kelas(kls_id)
+);
+
+CREATE Table Tugas(
     t_id                            char(8) NOT NULL,
     t_nama                          VARCHAR(30) NOT NULL,
     t_deskripsi                     Text ,
@@ -86,14 +86,9 @@ CREATE Table Detail_Tugas(
     t_tanggal_pengumpulan_terakhir  datetime ,
     t_file_dikumpulkan              VARCHAR(30) ,
     t_comment                       VARCHAR(30) ,
-    Sesi_Pertemuan_s_ID             char(8)     NOT NULL,
+    Sesi_Pertemuan_sp_ID             char(8)     NOT NULL,
     PRIMARY KEY(t_id),
-    FOREIGN KEY(Sesi_Pertemuan_s_ID) References Sesi_Pertemuan(s_ID)
+    FOREIGN KEY(Sesi_Pertemuan_sp_ID) References Sesi_Pertemuan(sp_ID)
 );
 
 COMMIT;
-
-
-
-
-
